9b09e875885200cea78526ea5ce66261ee492ebc
Anthony Salemo <ajssalemo@gmail.com>
2020-04-22 13:32:26 -0400

    updated templates/partials

--- pythondjango/pythondjango/urls.py
+++ pythondjango/pythondjango/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
-from .homepage.views import homepage
+from index.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', homepage, name="homepage"),
+    path('', index, name="index"),
 ]

178d9fda09234a78bea35a639acf7e73151b2996
Anthony Salemo <ajssalemo@gmail.com>
2020-05-02 17:47:18 -0400

    re-structured the side out menu, added dynami classes for styling

--- pythondjango/index/static/index/js/scripts.js
+++ pythondjango/index/static/index/js/scripts.js
@@ -1,5 +1,16 @@
-const openNav = () =>
-  (document.getElementById("mySidenav").style.width = "250px");
-
-const closeNav = () =>
-  (document.getElementById("mySidenav").style.width = "0");
+// Toggle option for a slide-out menu
+const openNav = () => {
+  if (document.getElementById("mySidenav").classList.contains("active")) {
+    document.getElementById("mySidenav").className =
+      "col-2 col-sm-1 col-md-1 col-lg-1 todo-page-side-column d-flex flex-column";
+    document.getElementById("sideNavContentOne").className = "d-none todo-page-sidenav-content";
+    document.getElementById("sideNavContentTwo").className = "d-none todo-page-sidenav-content";
+  } else {
+    document.getElementById("mySidenav").className =
+      "col-3 col-sm-2 col-md-2 col-lg-2 todo-page-side-column d-flex flex-column active";
+    document.getElementById("sideNavContentOne").className =
+      "d-block todo-page-sidenav-content pl-3";
+    document.getElementById("sideNavContentTwo").className =
+      "d-block todo-page-sidenav-content pl-3";
+  }
+};

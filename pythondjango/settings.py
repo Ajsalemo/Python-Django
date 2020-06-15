198ae6a82f8dce472df099b704bdd8306204e3b3
Anthony Salemo <ajssalemo@gmail.com>
2020-06-14 22:49:54 -0400

    editing Dockerfile for heroku deployment

--- pythondjango/settings.py
+++ pythondjango/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import environ
 import dj_database_url
+from django.core.management.utils import get_random_secret_key
 
 env = environ.Env()
 # reading .env file
@@ -26,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY", default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(False)
 ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS").split(" ")

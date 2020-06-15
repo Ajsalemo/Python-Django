3d394c01c97ac5937a40d3a41c865a44451cce8c
Anthony Salemo <ajssalemo@gmail.com>
2020-06-14 20:22:51 -0400

    attempted heroku deployment

--- pythondjango/settings.py
+++ pythondjango/settings.py
@@ -142,7 +142,7 @@ BOOTSTRAP4 = {
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 

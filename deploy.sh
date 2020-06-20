5b1237648857c966153d9b3983271d1931449e9d
Anthony Salemo <ajssalemo@gmail.com>
2020-06-20 16:26:57 -0400

    updating git-lab ci

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 
 ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
-  cd /pythondjango && mkdir pythondjango && cd pythondjango
+  cd /pythondjango 
   export $(cat .env | xargs)
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
-  cd ../pythondjango
   docker pull $IMAGE:postgres
   docker pull $IMAGE:web
   docker pull $IMAGE:nginx

a8b9a475ca6447c1bdac0676e969b49d6c48fd40
Anthony Salemo <ajssalemo@gmail.com>
2020-06-20 17:09:32 -0400

    updating git-lab ci

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
+#!/bin/sh
 
 ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
   cd /pythondjango 
   export $(cat .env | xargs)
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
+
   docker pull $IMAGE:postgres
   docker pull $IMAGE:web
   docker pull $IMAGE:nginx
--- setup_env.sh
+++ setup_env.sh
@@ -4,15 +4,15 @@ echo DEBUG=0 >> .env
 echo SQL_ENGINE=django.db.backends.postgresql >> .env
 echo DATABASE=postgres >> .env
 
-echo SECRET_KEY=$SECRET_KEY >> .env
-echo DB_DATABASE=$DB_DATABASE >> .env
-echo DB_USER=$DB_USER >> .env
-echo DB_PASSWORD=$DB_PASSWORD >> .env
-echo DB_HOST=$DB_HOST >> .env
-echo DB_PORT=$DB_PORT >> .env
-echo POSTGRES_USER=$POSTGRES_USER >> .env
-echo POSTGRES_PASSWORD=$POSTGRES_PASSWORD >> .env
-echo POSTGRES_DB=$POSTGRES_DB >> .env
+echo SECRET_KEY=')=sfsovy&@*+qdl^0*@yz9zm&x1_z%o*h@%-_%to$su!ob3*w-' >> .env
+echo DB_NAME=todo_db >> .env
+echo DB_USER=ajsalemo >> .env
+echo DB_PASSWORD=Dudebug1992 >> .env
+echo DB_HOST=db >> .env
+echo DB_PORT=5432 >> .env
+echo POSTGRES_USER=ajsalemo >> .env
+echo POSTGRES_PASSWORD=Dudebug1992 >> .env
+echo POSTGRES_DB=todo_db >> .env
 echo WEB_IMAGE=$IMAGE:web  >> .env
 echo NGINX_IMAGE=$IMAGE:nginx  >> .env
 echo POSTGRES_IMAGE=$IMAGE:postgres  >> .env

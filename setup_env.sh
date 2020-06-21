2c3de52286b5da047d70bca526c898d31867d22a
Anthony Salemo <ajssalemo@gmail.com>
2020-06-21 12:25:22 -0400

    added additional env variables to setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 echo DEBUG=0 >> .env
-echo SQL_ENGINE=django.db.backends.postgresql >> .env
 echo DATABASE=postgres >> .env
 
-echo SECRET_KEY=')=sfsovy&@*+qdl^0*@yz9zm&x1_z%o*h@%-_%to$su!ob3*w-' >> .env
-echo DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1] 174.138.52.28
-echo DB_ENGINE=django.db.backends.postgresql_psycopg2
-echo DB_NAME=todo_db >> .env
-echo DB_USER=ajsalemo >> .env
-echo DB_PASSWORD=Dudebug1992 >> .env
-echo DB_HOST=db >> .env
-echo DB_PORT=5432 >> .env
-echo POSTGRES_USER=ajsalemo >> .env
-echo POSTGRES_PASSWORD=Dudebug1992 >> .env
-echo POSTGRES_DB=todo_db >> .env
+echo SECRET_KEY=$SECRET_KEY >> .env
+echo DJANGO_ALLOWED_HOSTS=$DJANGO_ALLOWED_HOSTS
+echo DB_ENGINE=$DB_ENGINE
+echo DB_NAME=$DB_NAME >> .env
+echo DB_USER=$DB_USER >> .env
+echo DB_PASSWORD=$DB_PASSWORD >> .env
+echo DB_HOST=$DB_HOST >> .env
+echo DB_PORT=$DB_PORT >> .env
+echo POSTGRES_USER=$POSTGRES_USER >> .env
+echo POSTGRES_PASSWORD=$POSTGRES_PASSWORD >> .env
+echo POSTGRES_DB=$POSTGRES_DB >> .env
 echo WEB_IMAGE=$IMAGE:web  >> .env
 echo NGINX_IMAGE=$IMAGE:nginx  >> .env
 echo POSTGRES_IMAGE=$IMAGE:postgres  >> .env
